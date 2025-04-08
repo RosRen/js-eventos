600b6fa553dfe751a170b485bcda11f37ec208d6
rosario <18342114@uagro.mx>
2025-04-08 13:27:55 -0600

    Estructura de carpetas y codigo creado

--- js/main.js
+++ js/main.js
@@ -1,55 +1,60 @@
-const alumnos = []
+const alumnos = [];
 const Datos = document.querySelector("#agregar");
-document.addEventListener("keydown", event =>{
-    if (event.key === 'r' && event.ctrlKey){
+
+document.addEventListener("keydown", event => {
+    if (event.key === 'r' && event.ctrlKey) {
         event.preventDefault();
-        agregarAlumnos();}
-})
-Datos.addEventListener("click", event=>{
+        agregarAlumnos();
+    }
+});
+
+Datos.addEventListener("click", event => {
     event.preventDefault();
-    agregarAlumnos();}
-)
+    agregarAlumnos();
+});
 
-
-function agregarAlumnos(){
+function agregarAlumnos() {
     const nombre = prompt("Nombre: ");
     const grupo = prompt("Grupo");
     const matricula = prompt("Matricula: ");
 
     if (nombre.trim() === '' || grupo.trim() === '' || matricula.trim() === '') {
-        alert("Rellena todos los campos")
+        alert("Rellena todos los campos");
     } else {
-        alumnos.push({nombre, grupo, matricula});
+        alumnos.push({ nombre, grupo, matricula });
         alert("Alumno Registrado");
     }
- }
- const contenedor = document.querySelector("#informacion");
+}
+
+const contenedor = document.querySelector("#informacion");
 const Mostrar = document.querySelector("#mostrar");
-document.addEventListener("keydown", event =>{
-    if (event.key === 'm' && event.ctrlKey){
+
+document.addEventListener("keydown", event => {
+    if (event.key === 'm' && event.ctrlKey) {
         event.preventDefault();
-        mostrarAlumnos();}
-})
-Mostrar.addEventListener("click", event=>{
+        mostrarAlumnos();
+    }
+});
+
+Mostrar.addEventListener("click", event => {
     event.preventDefault();
     mostrarAlumnos();
-})
+});
 
-
-
-function mostrarAlumnos(){
-    if(contenedor.style.display ==="none"){
+function mostrarAlumnos() {
+    if (contenedor.style.display === "none") {
         contenedor.style.display = "block";
-        contenedor.innerHTML = ''
-    if (alumnos.length===0) {
-        contenedor.innerHTML='No hay alumnos registrados'
-    } else{
-        alumnos.forEach(alumno =>{
-            const p = document.createElement('p');
-        p.textContent= `Nombre: ${alumno.nombre}, Grupo:${alumno.grupo}, Matricula:${alumno.matricula}`;
-            contenedor.appendChild(p);
-        })
-    }}else{
+        contenedor.innerHTML = '';
+        if (alumnos.length === 0) {
+            contenedor.innerHTML = 'No hay alumnos registrados';
+        } else {
+            alumnos.forEach(alumno => {
+                const p = document.createElement('p');
+                p.textContent = `Nombre: ${alumno.nombre}, Grupo: ${alumno.grupo}, Matricula: ${alumno.matricula}`;
+                contenedor.appendChild(p);
+            });
+        }
+    } else {
         contenedor.style.display = "none";
     }
 }
