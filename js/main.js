9c90bf85aa2ff0afa3d6aada986847c3ad09ab33
rosario <18342114@uagro.mx>
2025-04-08 13:07:46 -0600

    Estructura de carpetas y codigo creado

--- js/main.js
+++ js/main.js
@@ -15,32 +15,32 @@ function agregarAlumnos(){
     const nombre = prompt("Nombre: ");
     const grupo = prompt("Grupo");
     const matricula = prompt("Matricula: ");
-    if (nombre && grupo && matricula){
+
+    if (nombre.trim() === '' || grupo.trim() === '' || matricula.trim() === '') {
+        alert("Rellena todos los campos")
+    } else {
         alumnos.push({nombre, grupo, matricula});
         alert("Alumno Registrado");
-    } 
-    
+    }
  }
-//
+ const contenedor = document.querySelector("#informacion");
 const Mostrar = document.querySelector("#mostrar");
 document.addEventListener("keydown", event =>{
     if (event.key === 'm' && event.ctrlKey){
         event.preventDefault();
-        document.querySelector("#informacion").style.display = "flex";
         mostrarAlumnos();}
 })
 Mostrar.addEventListener("click", event=>{
     event.preventDefault();
-    document.querySelector("#informacion").style.display = "flex";
     mostrarAlumnos();
 })
 
 
 
 function mostrarAlumnos(){
-    const contenedor = document.querySelector('#informacion');
-    contenedor.innerHTML='';
-
+    if(contenedor.style.display ==="none"){
+        contenedor.style.display = "block";
+        contenedor.innerHTML = ''
     if (alumnos.length===0) {
         contenedor.innerHTML='No hay alumnos registrados'
     } else{
@@ -49,5 +49,7 @@ function mostrarAlumnos(){
         p.textContent= `Nombre: ${alumno.nombre}, Grupo:${alumno.grupo}, Matricula:${alumno.matricula}`;
             contenedor.appendChild(p);
         })
+    }}else{
+        contenedor.style.display = "none";
     }
 }
